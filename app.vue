33da7030de13ec8980a7afa6c792f2e4639c4218
root <root@srv536697.hstgr.cloud>
2024-06-01 16:08:48 +0000

    init project

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    
+  </div>
+  <NuxtLayout class="layouts">
+    <NuxtPage />
+  </NuxtLayout>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,146 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <!-- <div>
+    <h1>Nuxt Routing set up successfully!</h1>
+    <p>Current route: {{ route.path }}</p>
+    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+  </div> -->
+  <main>
+			<div class="head-title">
+				<div class="left">
+					<h1>Admin Dashboard</h1>
+					<ul class="breadcrumb">
+						<li>
+							<a href="#">Dashboard</a>
+						</li>
+						<li><i class='bx bx-chevron-right' ></i></li>
+						<li>
+							<a class="active" href="#">Home</a>
+						</li>
+					</ul>
+				</div>
+				<a href="#" class="btn-download">
+					<i class='bx bxs-cloud-download' ></i>
+					<span class="text">Download PDF</span>
+				</a>
+			</div>
+
+			<ul class="box-info">
+				<li>
+					<i class='bx bx-transfer' ></i>
+					<span class="text">
+						<h3>1020</h3>
+						<p>Job Order</p>
+					</span>
+				</li>
+				<li>
+					<i class='bx bx-buildings'></i>
+					<span class="text">
+						<h3>2834</h3>
+						<p>Perusahaan</p>
+					</span>
+				</li>
+				<li>
+					<i class='bx bxs-group' ></i>
+					<span class="text">
+						<h3>2543</h3>
+						<p>Total PIC</p>
+					</span>
+				</li>
+			</ul>
+
+
+			<div class="table-data">
+				<div class="order">
+					<div class="head">
+						<h3>Job Order Terakhir</h3>
+						<i class='bx bx-search' ></i>
+						<i class='bx bx-filter' ></i>
+					</div>
+					<table>
+						<thead>
+							<tr>
+								<th>Pengguna</th>
+								<th>Waktu Pemeriksaan</th>
+								<th>Status</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>Andika</p>
+								</td>
+								<td>05-28-2024</td>
+								<td><span class="status completed">Completed</span></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>Abas</p>
+								</td>
+								<td>05-28-2024</td>
+								<td><span class="status pending">Pending</span></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>Ernando</p>
+								</td>
+								<td>05-28-2024</td>
+								<td><span class="status process">Process</span></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>Purwandaru</p>
+								</td>
+								<td>05-28-2024</td>
+								<td><span class="status pending">Pending</span></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>Nando</p>
+								</td>
+								<td>05-28-2024</td>
+								<td><span class="status completed">Completed</span></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="progres">
+					<div class="head">
+						<h3>Progres</h3>
+						<i class='bx bx-plus' ></i>
+						<i class='bx bx-filter' ></i>
+					</div>
+					<ul class="progres-list">
+						<li class="completed">
+							<p>Daftar Progres</p>
+							<i class='bx bx-dots-vertical-rounded' ></i>
+						</li>
+						<li class="completed">
+							<p>Daftar Progres</p>
+							<i class='bx bx-dots-vertical-rounded' ></i>
+						</li>
+						<li class="not-completed">
+							<p>Daftar Progres</p>
+							<i class='bx bx-dots-vertical-rounded' ></i>
+						</li>
+						<li class="completed">
+							<p>Daftar Progres</p>
+							<i class='bx bx-dots-vertical-rounded' ></i>
+						</li>
+						<li class="not-completed">
+							<p>Daftar Progres</p>
+							<i class='bx bx-dots-vertical-rounded' ></i>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</main>
+</template>
